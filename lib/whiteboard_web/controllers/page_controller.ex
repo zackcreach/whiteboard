8175a2fc897f7f5328048b5fb8a5a6bca1c2edbf
Zack Creach <zackcreach@gmail.com>
2025-04-01 08:16:01 -0400

    liveview boilerplate

---
+++ lib/whiteboard_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule WhiteboardWeb.PageController do
+  use WhiteboardWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
---
+++ lib/whiteboard_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule WhiteboardWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use WhiteboardWeb, :html
+
+  embed_templates "page_html/*"
+end
