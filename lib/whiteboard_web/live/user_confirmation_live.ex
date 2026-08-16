fb8bac97f6ccfe88d45704cd7b6c963f5de90ac6
Zack Creach <zackcreach@gmail.com>
2026-08-15 23:10:38 -0400

    increase confirmation email padding

--- lib/whiteboard_web/live/user_confirmation_live.ex
+++ lib/whiteboard_web/live/user_confirmation_live.ex
@@ -12,7 +12,7 @@ defmodule WhiteboardWeb.UserConfirmationLive do
         <h3>Confirm email</h3>
         <p
           :if={@email}
-          class="my-4 rounded-lg border border-stone-300 bg-stone-50 px-4 py-3 text-center text-sm font-semibold text-stone-700 dark:border-stone-600 dark:bg-stone-800 dark:text-stone-200"
+          class="my-4 rounded-lg border border-stone-300 bg-stone-50 px-4 py-5 text-center text-sm font-semibold text-stone-700 dark:border-stone-600 dark:bg-stone-800 dark:text-stone-200"
         >
           {@email}
         </p>
